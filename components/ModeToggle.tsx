1496463715890cbf2b9745b68545eaf1ad8a4b29
zhangkuo <769329489@qq.com>
2024-12-09 01:02:10 +0800

    dark

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -3,14 +3,20 @@
 import { motion } from 'framer-motion'
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from 'react';
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
 
   return (
     <motion.button
       className="p-2 rounded-full  bg-zinc-400 dark:bg-slate-200/75"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
     >
