aa46a27bbc8a56080d45cb05458bb64c60ecdc89
zhangkuo <769329489@qq.com>
2024-12-09 00:50:27 +0800

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from "../components/ThemeProvider";
 import './globals.css'
-import { Viewport } from "next";
 
 import * as React from "react"
 
 
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
 export const metadata = {
   title: 'My Personal Blog',
   description: 'Welcome to my personal blog and portfolio',
@@ -26,9 +19,8 @@ export default function RootLayout({
       <body>
         <ThemeProvider          
           attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange>
+          defaultTheme="system"
+          enableSystem>
           {children}
         </ThemeProvider>
       </body>
