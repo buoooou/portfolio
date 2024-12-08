c26e437e1e044be81ecfac252e54377e0ccfbb83
zhangkuo <769329489@qq.com>
2024-12-09 00:02:49 +0800

    blog

--- config/site.ts
+++ config/site.ts
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
   ogImage: "https://buouui.com/og.jpg",
   links: {
     twitter: "https://x.com/intent/follow?screen_name=buoooou0",
-    github: "https://github.com/buoooou",
+    github: "https://github.com/buoooou/portfolio",
     tiktok: "https://www.tiktok.com/@buoooou",
     thread: "https://www.threads.net/@zhangkuo92",
     ins: "https://www.instagram.com/zhangkuo92",
